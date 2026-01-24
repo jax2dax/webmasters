3a6243a1ee31128e15e7c7e9b8c7b8f0aae5e937
Jash <joshuabelay9922@gmail.com>
2026-01-23 17:11:37 -0800

    shadcn badge

--- app/explore/page.jsx
+++ app/explore/page.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import { CardImage } from "../../components/CardImg"
+
+
 const explore = () => {
   return (
     <>
     Explore
+    <div className='flex '>
+      <CardImage /> 
+      <CardImage /> 
+      
+    </div>
     </>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,16 @@ const page = () => {
   return (
     <>
     <section className="container ">pagevlkm
-        <h1>H-O-M-E</h1>
+        <h1></h1>
+        <div className="" style={{height:"200px", width:"200px", backgroundColor:"red"}}>   
+
+        </div>
+        <div className="bg-blue-500" style={{height:"200px", width:"200px", backgroundColor:"blue"}}>   
+
+        </div>
+        <div className="bg-green-500" style={{height:"200px", width:"200px", backgroundColor:"green"}}>   
+
+        </div>
         
     </section>
     
