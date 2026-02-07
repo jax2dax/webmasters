b3ae95af03ac9928baec2143a00357d895de52b4
Jash <joshuabelay9922@gmail.com>
2026-02-06 23:19:00 -0800

    programs

--- app/explore/page.jsx
+++ app/explore/page.jsx
@@ -10,6 +10,7 @@ import PPLwave from '../../lib/animation/peoplewave'
 import { Separator } from "@/components/ui/separator"
 import { BowArrow } from 'lucide-react'
 import { BriefcaseBusiness } from 'lucide-react'
+import { Blocks } from 'lucide-react';
 
 /**sercive Card*/
 
@@ -33,7 +34,7 @@ const explore = () => {
     <div className="flex max-w-[100vw] justify-center mb-0 pb-0"><span className="text-8xl text-green-500 mb-0 pb-0 translate-y-11 max-sm:text-4xl font-serif max-sm:mb-7 ">Navigate what we have up!</span></div>
     <PPLwave />
     <h1 className="sm:ml-20">Services</h1>
-    <div className="flex z-0 gap-2 my-3 z-0 border-b-green-300 border-t-green-300 overflow-hidden border-t-8 border-b-8  border-solid w-max-[2000px] justify-evenly items-center mt-13 py-16 color-gradient1 ">
+    <div className="flex  gap-2 my-3 z-0 border-b-green-300 border-t-green-300 overflow-hidden border-t-8 border-b-8  border-solid w-max-[2000px] justify-evenly items-center mt-13 py-16 color-gradient1 ">
       <CardImage2 
             name={"..."}
               badge={"..."}
@@ -140,7 +141,8 @@ const explore = () => {
     
 
     <div className="w- my-20 h-auto border-2 border-solid hover:translate-y-1 pl-4 rounded-2xl pb-1.5 pt-0.5 mx-4"><Link href="/learn-more#evaluation">
-      <h1><BowArrow width={30} height={30} /> Discover How we Filter and Provide quality service </h1>
+      <h1><BowArrow width={30} height={30} /> Discover How we Filter and Provide quality service </h1> 
+       <Separator />
     <p className="text-gray-600">We dont just evaluate based on big names and popularity, we dig deep into websites 
       and perform a series of inspections that will help us evaluate if it lives upto its name...</p>
       </Link>
@@ -157,6 +159,17 @@ const explore = () => {
         <span className="bg-green-200 text-black hover:scale-2 px-20 text-3xl"><Link href="/apply" >Apply
         </Link></span>
       </div>
+
+      <div className="w- my-20 h-auto border-2 border-solid hover:translate-y-1 pl-4 rounded-2xl pb-1.5 pt-0.5 mx-4"><Link href="/programs">
+      <h1><Blocks width={30} height={30} />Programs and Affiliates</h1>
+    <p className="text-gray-600">
+       <Separator />
+      Join our Latest programs and learn about our affiliates. 
+      
+    </p>
+     <Separator />
+      </Link>
+    </div>
 
 
 
